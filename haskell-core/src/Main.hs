3ee3edb4520373791e6e67ca620a0b8bab497910
Ana Paola Oviedo <anaovied015@hotmail.com>
2025-10-09 10:18:31 -0600

    still setting up

--- haskell-core/src/Main.hs
+++ haskell-core/src/Main.hs
@@ -1,1 +1,21 @@
 --my actual algorithms will live here 
+
+import Cathedral.Core.Types
+
+-- ← ALGORITHM 1: Merge Sort
+mergeSort :: Algorithm
+mergeSort = Algorithm
+    { algName = "Merge Sort"
+    , algParadigm = DivideAndConquer
+    , algComplexity = Linearithmic
+    , algDescription = "Split array, sort halves, merge"
+    }
+
+-- ← ALGORITHM 2: Binary Search
+binarySearch :: Algorithm
+binarySearch = Algorithm
+    { algName = "Binary Search"
+    , algParadigm = DivideAndConquer
+    , algComplexity = Logarithmic
+    , algDescription = "Check middle, search half"
+    }
