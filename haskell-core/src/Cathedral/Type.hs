eca946357a4043de86c65454e403264d7ef16b45
Ana Paola Oviedo <anaovied015@hotmail.com>
2025-10-09 10:00:39 -0600

    defining the algorithms

--- haskell-core/src/Cathedral/Type.hs
+++ haskell-core/src/Cathedral/Type.hs
@@ -1,1 +1,18 @@
 --here is where de definition of 'algorithm' will be
+-- The shape of an Algorithm
+data Algorithm = Algorithm
+    { algName :: Name
+    , algParadigm :: Paradigm
+    , algComplexity :: Complexity
+    --, algDescription :: Description
+    }
+
+-- The paradigms
+data Paradigm = DivideAndConquer | Greedy | DynamicProgramming | BruteForce | Backtracking 
+
+-- The complexity classes
+data Complexity = Constant | Linear | Cuadraric | Exponential | Logarithmic 
+
+data Name = Dijkstra | Lomuto | BubbleSort | QuickSort | MergeSort | MedianOfMedians | Karatsuab 
+
+--data Description = 
