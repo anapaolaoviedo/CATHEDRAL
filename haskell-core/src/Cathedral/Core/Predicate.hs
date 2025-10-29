bc3d5ded7b32cd772bfdbb0feb47ea83ece4217a
Ana Paola Oviedo <anaovied015@hotmail.com>
2025-10-29 12:57:32 -0600

    first version of predicates

--- haskell-core/src/Cathedral/Core/Predicate.hs
+++ haskell-core/src/Cathedral/Core/Predicate.hs
@@ -1,28 +1,55 @@
 module Cathedral.Core.Types where
 
--- | Algorithmic paradigms
-data Paradigm
-    = DivideAndConquer
-    | Greedy
-    | DynamicProgramming
-    | BruteForce
-    | GraphTraversal
-    deriving (Eq, Show)
+-- here i need to add the predicates. i wanna keep it rooted into logic 
+-- fro now im gonna keep it simple 
 
--- | Complexity classes
-data Complexity
-    = Constant          -- O(1)
-    | Logarithmic       -- O(log n)
-    | Linear            -- O(n)
-    | Linearithmic      -- O(n log n)
-    | Quadratic         -- O(n²)
-    | Exponential       -- O(2^n)
-    deriving (Eq, Show, Ord)
+type Predicate a = a -> Bool 
 
--- | Algorithm specification
-data Algorithm = Algorithm
-    { algName        :: String
-    , algParadigm    :: Paradigm
-    , algComplexity  :: Complexity
-    , algDescription :: String
-    } deriving (Eq, Show)
+--precondition of what must be true before BEFORE the algorithm runs 
+newtype Precondition a = Precondition (Predicate a)
+    deriving (Show)
+
+--postcondition is what must be true after the algorithm runs 
+newtype Postcondition a b = Postcondition (a -> b -> Bool)
+    deriving (Show)
+
+--invariant is what bust me true during the algorithm 
+-- for now just a string 
+newtype Invariant = Invariant String 
+    deriving (Show, Eq)
+
+-- helper to create a precondition form a predicate 
+mkPrecondition :: (a -> Bool)-> Precondition a 
+mkPrecondition = Precondition 
+
+--helper to create a postcondition from a two arguement predicate 
+mkPostcondition :: (a -> b -> Bool) -> Postcondition a b 
+mkPostcondition = Postcondition 
+
+--helper to create an invariant from description
+mkInvariant :: String -> Invariant 
+mkInvariant = Invariant 
+
+--helper always true predicate
+alwaysTrue :: Predicate a 
+alwaysTrue = const True 
+
+--helper always flase predicate 
+alwaysFalse :: Predicate a
+alwaysFalse = const False 
+
+--helper combine two predicates with AND 
+andPred :: Predicate a -> Predicate a -> Predicate a 
+andPred p1 p2 = \x -> p1 x && p2 x 
+
+--helper combine two predicates with OR 
+ordPred :: Predicate a -> Predicate a -> Predicate a 
+ordPred p1 p2 = \x -> p1 x || p2 x 
+
+-- helper to negate a predicate
+notPred :: Predicate a -> Predicate a
+notPred p = \x -> not (p x)
+
+
+
+
